3dd0b003736c12e553528796e8a48dbe96e1d359
s33493 <s33493@pjwstk.edu.pl>
2025-03-16 18:08:24 +0100

    Update and rename main.java to zad1.java

--- zad1.java
+++ zad1.java
@@ -1,4 +1,4 @@
-public class main {
+public class zad1 {
     public static void main(String[] args) {
     int[]arr={1,2,3,4,5,6};
     System.out.println(java.util.Arrays.toString(Filterevenindexodd(arr)));
