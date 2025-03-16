58ec2be8d9a256631f76907e83a8f904185d4e01
s33493 <s33493@pjwstk.edu.pl>
2025-03-16 12:17:50 +0100

    cw1

--- main.java
+++ main.java
@@ -1,5 +1,22 @@
 public class main {
     public static void main(String[] args) {
-
+    int[]arr={1,2,3,4,5,6};
+    System.out.println(java.util.Arrays.toString(metoda(arr)));
+    }
+    public static int[]metoda(int[]arr){
+        int counter=0;
+        for(int i=0;i<arr.length;i+=2){
+            if(arr[i]%2!=0){
+                counter++;
+            }
+        }
+        int[] result=new int[counter];
+        int index=0;
+        for(int i=0;i<arr.length;i+=2){
+            if(arr[i]%2!=0){
+                result[index++]=arr[i];
+            }
+        }
+        return result;
     }
 }
